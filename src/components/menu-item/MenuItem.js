ad995f3623626bebd511963cf67e5632f5dd7266
martyumlas@gmail.com <martyumlas@gmail.com>
2021-11-09 11:39:28 +0800

    styling menus

--- src/components/menu-item/MenuItem.js
+++ src/components/menu-item/MenuItem.js
@@ -2,12 +2,16 @@ import './MenuItem.scss'
 
 const MenuItem = ({title, imageUrl, size}) => {
     return (
-        <div style={{backgroundImage: `url(${imageUrl})`}} className={`menu-item ${size}`}>
+        <div className={`menu-item ${size}`}>
+            
+            <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} ></div>
+
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
             </div>
-        </div>
+            </div>
+           
     )
 }
 
