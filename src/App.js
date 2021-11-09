21502904dc824a245ceaa690069e16ba5a039195
martyumlas@gmail.com <martyumlas@gmail.com>
2021-11-09 19:35:47 +0800

    header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
 import Shop from './pages/shop/Shop'
+import Header  from './components/header/Header';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='shop' element={<Shop/>}/>
-    </Routes>
+    <Fragment>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='shop' element={<Shop/>}/>
+      </Routes>
+    </Fragment>
   );
 }
 
