2215b283ceb32fe03ac125bde53dd5b75a3a301c
martyumlas@gmail.com <martyumlas@gmail.com>
2021-11-22 09:34:09 +0800

    cart dropdown component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,29 +4,34 @@ import { ReactComponent as Logo} from '../../assets/crown.svg'
 import { signOutWithGoogle } from '../../firebase/firebase'
 import {connect} from 'react-redux'
 import CartIcon from '../cart-icon/CartIcon'
+import CartDropdown from '../cart-dropdown/CartDropdown'
 
 const Header = ({currentUser}) => {
     return (
         <div className='header'>
-           <Link to='/'>
-               <Logo/>
-           </Link>
-           <div className="options">
-               <Link className='option' to='shop'>
-                   SHOP
-               </Link>
-               <Link className='option' to='contact'>
-                   CONTACT
-               </Link>
-              {
-                 currentUser ? 
-                  (<div className='option' onClick={signOutWithGoogle}>SIGN OUT</div>)
-                  :
-                  (<Link className='option' to='signin'>SIGN IN</Link>)
-              }
+            <Link to='/'>
+                <Logo/>
+            </Link>
+            <div className="options">
+                <Link className='option' to='shop'>
+                    SHOP
+                </Link>
+                <Link className='option' to='contact'>
+                    CONTACT
+                </Link>
+                {
+                    currentUser ? 
+                    (<div className='option' onClick={signOutWithGoogle}>SIGN OUT</div>)
+                    :
+                    (<Link className='option' to='signin'>SIGN IN</Link>)
+                }
 
-              <CartIcon />
-           </div>
+                <CartIcon />
+            </div>
+
+            <CartDropdown />
+            
+
         </div>
     )
 }
