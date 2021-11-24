6417dc4246cc1488908e6145357947c1a3344b29
martyumlas@gmail.com <martyumlas@gmail.com>
2021-11-24 11:27:58 +0800

    update logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger, ]
+const middlewares = []
+
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, composeEnhancers( applyMiddleware(...middlewares)))
 
