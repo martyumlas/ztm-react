17d3aae8cbdb9bc146d85879b899ddc6b3fc0392
martyumlas@gmail.com <martyumlas@gmail.com>
2021-11-28 19:04:18 +0800

    clear cart on sign out

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects'
+import { takeLatest, call, put, all } from 'redux-saga/effects'
 import ShopActionTypes from './shop.types'
 import { db, convertCollectionSnapShotToMap } from '../../firebase/firebase'
 import { collection, getDocs } from '@firebase/firestore'
@@ -20,4 +20,10 @@ export function* fetchCollectionsStartAsync() {
 
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsStartAsync)
+}
+
+export function* shopSagas() {
+    yield all([
+        call(fetchCollectionsStart)
+    ])
 }
